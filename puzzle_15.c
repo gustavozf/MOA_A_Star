31fe571aaf27d1a7f3dbd3e49b7acfdeeada1d7e
gustavozf <gzf1996@gmail.com>
2017-10-30 20:28:00 -0200

    Retorno = heuristica 2

--- puzzle_15.c
+++ puzzle_15.c
@@ -15,12 +15,39 @@ int h_linha_1(){
   pecas_fora = i = j = 0;
 
   while(i < 16){
+    if(tabuleiro[i] != tabuleiro_correto[j]){
+      if (tabuleiro[i] != 0){
+        pecas_fora++;
+      }
+    }
+    i++;
+    j++;
+  }
+
+  return pecas_fora;
+}
+
+int h_linha_2(){
+  //i = peça da frente
+  //j = peca de tras
+  int pecas_fora, i, j;
+
+  pecas_fora = j = 0;
+  i = 1;
+  while(i <16){
     if (tabuleiro[i] == 0){
       i++;
     }
-    if(tabuleiro[i] != tabuleiro_correto[j]){
+    if (tabuleiro[j] == 0){
+      j++;
+    }
+    if(tabuleiro[i] == tabuleiro[j]){
+      i++;
+    }
+    if(tabuleiro[i] != (tabuleiro[j]+1)){
       pecas_fora++;
     }
+
     i++;
     j++;
   }
@@ -76,16 +103,23 @@ void organiza_espiral(int entrada[][4]){
 }
 
 void le_tabuleiro(){
-  int i, j, entrada[4][4];
+  //int i, j;
+  int in[4][4];
 
-  for (i = 0; i < 4; i++) {
+  scanf("%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d",
+            &in[0][0], &in[0][1], &in[0][2], &in[0][3],
+            &in[1][0], &in[1][1], &in[1][2], &in[1][3],
+            &in[2][0], &in[2][1], &in[2][2], &in[2][3],
+            &in[3][0], &in[3][1], &in[3][2], &in[3][3]);
+
+  /*for (i = 0; i < 4; i++) {
     for(j=0; j< 4; j++){
       printf("\nInsira a peça A[%d, %d]: ", i+1, j+1);
       scanf("%d", &entrada[i][j]);
     }
-  }
-  print_entrada(entrada);
-  organiza_espiral(entrada);
+  }*/
+  //print_entrada(entrada);
+  organiza_espiral(in);
 }
 
 //Le o tabuleiro do arquivo e salva na memoria
@@ -115,13 +149,17 @@ void le_tabuleiro(){
 }*/
 
 int main(){
-  int h1;
+  //int h1;
+  int h2;
 
   le_tabuleiro();
-  printf("Valores organizados:\n0 1 2 3 7 11 15 14 13 12 8 4 5 6 10 9\n");
-  print_inline_tabuleiro();
-  h1 = h_linha_1();
-  printf("\nA heuristica #1 retornou o valor: %d\n", h1);
+  //print_inline_tabuleiro();
 
-  return h1;
+  //h1 = h_linha_1();
+  //printf("%d", h1);
+
+  h2 = h_linha_2();
+  printf("%d", h2);
+
+  return 0;
 }
